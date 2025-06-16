0f401a42f25f56731112b3dc124c0808e99c22c6
shivesh trivedi <shivesh.trivedi@intimetec.com>
2025-06-16 09:22:46 +0530

    adding console project

---
+++ News_aggregator_console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace News_aggregator_console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ news_aggregator.console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace news_aggregator.console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is it working?");
+        }
+    }
+}
