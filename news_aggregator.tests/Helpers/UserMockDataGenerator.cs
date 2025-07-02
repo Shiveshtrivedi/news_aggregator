16b3d3018660b872ef7f8818d199e9e1c318aa59
shivesh trivedi <shivesh.trivedi@intimetec.com>
2025-07-02 11:10:38 +0530

    implement unit tests for AuthService

---
+++ news_aggregator.tests/Helpers/UserMockDataGenerator.cs
@@ -0,0 +1,56 @@
+﻿using news_aggregator.domain.Models.DTOs;
+using news_application.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace news_aggregator.tests.Helpers
+{
+    public class UserMockDataGenerator
+    {
+        public static UserDTO GetTestUserDto()
+        {
+            return new UserDTO
+            {
+                UserId = 1,
+                UserName = "TestUser",
+                Email = "testuser@example.com",
+                Password = "Password123",
+                Role = UserRole.User,
+                Token = "mocked-jwt-token"
+            };
+        }
+
+        public static LoginDTO GetValidLoginDto()
+        {
+            return new LoginDTO
+            {
+                Email = "testuser@example.com",
+                Password = "Password123"
+            };
+        }
+
+        public static LoginDTO GetInvalidLoginDto()
+        {
+            return new LoginDTO
+            {
+                Email = "wrong@example.com",
+                Password = "WrongPassword"
+            };
+        }
+
+        public static LoginResponseDto GetLoginResponseDto()
+        {
+            return new LoginResponseDto
+            {
+                UserId = 1,
+                UserName = "TestUser",
+                Email = "testuser@example.com",
+                Role = UserRole.User,
+                Token = "mocked-jwt-token"
+            };
+        }
+    }
+}
